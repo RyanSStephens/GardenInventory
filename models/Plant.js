83bd40d328a73b360b03517ab626df3159770886
Riboflavin <me@ryanstephens.work>
2020-06-20 11:28:00 -0400

    refactor: enhance Plant model with better validation and methods
    - Add comprehensive field validation with custom messages
    - Implement virtual properties for growth tracking
    - Add database indexes for query optimization
    - Include pre-save middleware for status management
    - Add static and instance methods for common operations

--- models/Plant.js
+++ models/Plant.js
@@ -1,52 +1,167 @@
 const mongoose = require('mongoose');
 
-const PlantSchema = new mongoose.Schema({
+const plantSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    trim: true
+    required: [true, 'Plant name is required'],
+    trim: true,
+    maxlength: [100, 'Plant name cannot exceed 100 characters']
   },
   variety: {
     type: String,
-    trim: true
+    trim: true,
+    maxlength: [50, 'Variety name cannot exceed 50 characters']
   },
   category: {
     type: String,
-    required: true,
-    enum: ['vegetable', 'fruit', 'herb', 'flower', 'tree', 'shrub']
-  },
-  plantedDate: {
-    type: Date,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
+    required: [true, 'Category is required'],
+    enum: {
+      values: ['vegetables', 'fruits', 'herbs', 'flowers', 'trees', 'shrubs', 'other'],
+      message: 'Category must be one of: vegetables, fruits, herbs, flowers, trees, shrubs, other'
+    }
   },
   status: {
     type: String,
-    enum: ['seed', 'seedling', 'growing', 'flowering', 'fruiting', 'harvested', 'dormant'],
-    default: 'seed'
+    enum: {
+      values: ['planted', 'seedling', 'growing', 'flowering', 'fruiting', 'harvested', 'dormant', 'failed'],
+      message: 'Status must be one of: planted, seedling, growing, flowering, fruiting, harvested, dormant, failed'
+    },
+    default: 'planted'
   },
-  notes: {
-    type: String
+  plantedDate: {
+    type: Date,
+    required: [true, 'Planted date is required'],
+    validate: {
+      validator: function(date) {
+        return date <= new Date();
+      },
+      message: 'Planted date cannot be in the future'
+    }
   },
   expectedHarvestDate: {
+    type: Date,
+    validate: {
+      validator: function(date) {
+        return !date || date > this.plantedDate;
+      },
+      message: 'Expected harvest date must be after planted date'
+    }
+  },
+  harvestDate: {
+    type: Date,
+    validate: {
+      validator: function(date) {
+        return !date || date >= this.plantedDate;
+      },
+      message: 'Harvest date must be on or after planted date'
+    }
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: [100, 'Location cannot exceed 100 characters']
+  },
+  notes: {
+    type: String,
+    trim: true,
+    maxlength: [500, 'Notes cannot exceed 500 characters']
+  },
+  careInstructions: {
+    type: String,
+    trim: true,
+    maxlength: [300, 'Care instructions cannot exceed 300 characters']
+  },
+  lastCareDate: {
     type: Date
   },
-  wateringSchedule: {
-    frequency: String,
-    lastWatered: Date
+  isActive: {
+    type: Boolean,
+    default: true
   },
-  fertilizing: {
-    type: String
-  },
-  companions: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Plant'
+  tags: [{
+    type: String,
+    trim: true,
+    maxlength: [30, 'Tag cannot exceed 30 characters']
   }]
 }, {
-  timestamps: true
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
-module.exports = mongoose.model('Plant', PlantSchema); 
+// Virtual for days since planted
+plantSchema.virtual('daysSincePlanted').get(function() {
+  if (!this.plantedDate) return null;
+  const diffTime = Math.abs(new Date() - this.plantedDate);
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+});
+
+// Virtual for days until expected harvest
+plantSchema.virtual('daysUntilHarvest').get(function() {
+  if (!this.expectedHarvestDate) return null;
+  const diffTime = this.expectedHarvestDate - new Date();
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+});
+
+// Virtual for growth stage percentage (rough estimate)
+plantSchema.virtual('growthProgress').get(function() {
+  if (!this.plantedDate || !this.expectedHarvestDate) return null;
+  const totalTime = this.expectedHarvestDate - this.plantedDate;
+  const elapsedTime = new Date() - this.plantedDate;
+  const progress = Math.min(100, Math.max(0, (elapsedTime / totalTime) * 100));
+  return Math.round(progress);
+});
+
+// Index for common queries
+plantSchema.index({ category: 1, status: 1 });
+plantSchema.index({ plantedDate: -1 });
+plantSchema.index({ expectedHarvestDate: 1 });
+plantSchema.index({ location: 1 });
+
+// Pre-save middleware
+plantSchema.pre('save', function(next) {
+  // Auto-update status based on harvest date
+  if (this.harvestDate && this.status !== 'harvested') {
+    this.status = 'harvested';
+  }
+  
+  // Ensure consistent data
+  if (this.status === 'failed' || this.status === 'harvested') {
+    this.isActive = false;
+  }
+  
+  next();
+});
+
+// Static methods
+plantSchema.statics.getActiveCount = function() {
+  return this.countDocuments({ isActive: true });
+};
+
+plantSchema.statics.getByCategory = function(category) {
+  return this.find({ category, isActive: true }).sort({ plantedDate: -1 });
+};
+
+plantSchema.statics.getHarvestReady = function() {
+  const today = new Date();
+  return this.find({
+    expectedHarvestDate: { $lte: today },
+    status: { $nin: ['harvested', 'failed'] },
+    isActive: true
+  }).sort({ expectedHarvestDate: 1 });
+};
+
+// Instance methods
+plantSchema.methods.markAsHarvested = function(harvestDate = new Date()) {
+  this.status = 'harvested';
+  this.harvestDate = harvestDate;
+  this.isActive = false;
+  return this.save();
+};
+
+plantSchema.methods.updateCareDate = function() {
+  this.lastCareDate = new Date();
+  return this.save();
+};
+
+module.exports = mongoose.model('Plant', plantSchema); 
