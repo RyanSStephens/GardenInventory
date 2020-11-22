c10644ae4cf58ea3e1f0429a2c47172365dbc401
Riboflavin <me@ryanstephens.work>
2020-11-22 13:30:00 -0500

    security: enhance server security and error handling
    - Add comprehensive helmet configuration with CSP
    - Implement rate limiting for API endpoints
    - Add graceful shutdown handlers for SIGTERM/SIGINT
    - Improve error handling with specific error types
    - Add API documentation endpoint and health checks
    - Configure CORS properly for production

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const compression = require('compression');
 const path = require('path');
 require('dotenv').config();
 
@@ -11,9 +15,47 @@ const logger = require('./utils/logger');
 const { requestMetrics, errorTracking } = require('./middleware/monitoring');
 const scheduler = require('./utils/scheduler');
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Security middleware
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'"],
+      imgSrc: ["'self'", "data:", "https:"],
+      connectSrc: ["'self'"],
+      fontSrc: ["'self'"],
+      objectSrc: ["'none'"],
+      mediaSrc: ["'self'"],
+      frameSrc: ["'none'"],
+    },
+  },
+}));
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use('/api/', limiter);
+
+// Performance middleware
+app.use(compression());
+
+// CORS configuration
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL : true,
+  credentials: true
+}));
+
+// Body parsing middleware
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Add monitoring middleware
@@ -40,6 +82,14 @@ const monitoringRoutes = require('./routes/monitoring');
 const notificationRoutes = require('./routes/notifications');
 const backupRoutes = require('./routes/backup');
 
+// Performance monitoring middleware
+const performanceMiddleware = require('./middleware/performance');
+app.use(performanceMiddleware);
+
+// Monitoring middleware
+const monitoringMiddleware = require('./middleware/monitoring');
+app.use(monitoringMiddleware);
+
 // Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -54,17 +104,118 @@ app.use('/api/monitoring', monitoringRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/backup', backupRoutes);
 
-// Error handling middleware (must be last)
-app.use(errorTracking);
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+    memory: process.memoryUsage(),
+    version: process.env.npm_package_version || '1.0.0'
+  });
+});
 
-app.listen(PORT, () => {
+// API documentation endpoint
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Garden Inventory API',
+    version: '1.0.0',
+    endpoints: {
+      plants: '/api/plants',
+      inventory: '/api/inventory',
+      harvests: '/api/harvests',
+      dashboard: '/api/dashboard',
+      monitoring: '/api/monitoring',
+      notifications: '/api/notifications',
+      backup: '/api/backup'
+    },
+    documentation: '/docs/API.md'
+  });
+});
+
+// Serve index.html for all non-API routes (SPA support)
+app.get('*', (req, res) => {
+  if (!req.path.startsWith('/api/')) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  } else {
+    res.status(404).json({ error: 'API endpoint not found' });
+  }
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const errors = Object.values(err.errors).map(e => e.message);
+    return res.status(400).json({
+      success: false,
+      error: 'Validation Error',
+      details: errors
+    });
+  }
+  
+  // Mongoose cast error (invalid ObjectId)
+  if (err.name === 'CastError') {
+    return res.status(400).json({
+      success: false,
+      error: 'Invalid ID format'
+    });
+  }
+  
+  // Duplicate key error
+  if (err.code === 11000) {
+    return res.status(400).json({
+      success: false,
+      error: 'Duplicate entry'
+    });
+  }
+  
+  // Default error
+  res.status(err.status || 500).json({
+    success: false,
+    error: err.message || 'Internal Server Error'
+  });
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    error: 'Route not found'
+  });
+});
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received, shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated');
+    mongoose.connection.close();
+  });
+});
+
+process.on('SIGINT', () => {
+  console.log('SIGINT received, shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated');
+    mongoose.connection.close();
+  });
+});
+
+const server = app.listen(PORT, () => {
   logger.info('Server started', { 
     port: PORT, 
     environment: process.env.NODE_ENV || 'development',
     timestamp: new Date().toISOString()
   });
-  console.log(`Garden Inventory server running on port ${PORT}`);
+  console.log(`🌱 Garden Inventory Server running on port ${PORT}`);
+  console.log(`📊 Health check: http://localhost:${PORT}/health`);
+  console.log(`📖 API docs: http://localhost:${PORT}/api`);
   
   // Initialize task scheduler
   scheduler.initialize();
-}); 
+});
+
+module.exports = app; 
