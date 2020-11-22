91e2524a27996aec34a13c6abc85b99b0732d215
Riboflavin <me@ryanstephens.work>
2020-11-22 11:15:00 -0500

    fix: relax CSP and security headers for development
    - Allow unsafe-inline for scripts to fix frontend issues
    - Disable HSTS for local development
    - Remove overly restrictive CORS policies
    - Maintain essential security while allowing development workflow

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
-      scriptSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'"],
       fontSrc: ["'self'"],
@@ -30,21 +30,13 @@ app.use(helmet({
       frameSrc: ["'none'"]
     }
   },
-  crossOriginEmbedderPolicy: true,
-  crossOriginOpenerPolicy: true,
   crossOriginResourcePolicy: { policy: "cross-origin" },
   dnsPrefetchControl: true,
   frameguard: { action: 'deny' },
   hidePoweredBy: true,
-  hsts: {
-    maxAge: 31536000,
-    includeSubDomains: true,
-    preload: true
-  },
+  hsts: false, // Disable for development
   ieNoOpen: true,
   noSniff: true,
-  originAgentCluster: true,
-  permittedCrossDomainPolicies: false,
   referrerPolicy: { policy: "no-referrer" },
   xssFilter: true
 }));
