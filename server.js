9242173b6bf85cb8107879684cabc04ab6f56fb6
Riboflavin <me@ryanstephens.work>
2020-11-22 10:30:00 -0500

    feat: enhance security headers with comprehensive helmet configuration
    - Add strict Content Security Policy
    - Enable Cross-Origin policies for better isolation
    - Configure HSTS with preload for HTTPS enforcement
    - Add comprehensive security headers for production deployment

--- server.js
+++ server.js
@@ -27,9 +27,26 @@ app.use(helmet({
       fontSrc: ["'self'"],
       objectSrc: ["'none'"],
       mediaSrc: ["'self'"],
-      frameSrc: ["'none'"],
-    },
+      frameSrc: ["'none'"]
+    }
   },
+  crossOriginEmbedderPolicy: true,
+  crossOriginOpenerPolicy: true,
+  crossOriginResourcePolicy: { policy: "cross-origin" },
+  dnsPrefetchControl: true,
+  frameguard: { action: 'deny' },
+  hidePoweredBy: true,
+  hsts: {
+    maxAge: 31536000,
+    includeSubDomains: true,
+    preload: true
+  },
+  ieNoOpen: true,
+  noSniff: true,
+  originAgentCluster: true,
+  permittedCrossDomainPolicies: false,
+  referrerPolicy: { policy: "no-referrer" },
+  xssFilter: true
 }));
 
 // Rate limiting
